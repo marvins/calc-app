20aff1754f9cb5b149cf8bcf7752bd5186890bf5
Marvin Smith <marvin.smith@terminus-geo.com>
2025-03-21 11:09:54 -0600

    Starting legit work on C++ version.

---
+++ conanfile.py
@@ -0,0 +1,73 @@
+#**************************** INTELLECTUAL PROPERTY RIGHTS ****************************#
+#*                                                                                    *#
+#*                           Copyright (c) 2025 Terminus LLC                          *#
+#*                                                                                    *#
+#*                                All Rights Reserved.                                *#
+#*                                                                                    *#
+#*          Use of this source code is governed by LICENSE in the repo root.          *#
+#*                                                                                    *#
+#**************************** INTELLECTUAL PROPERTY RIGHTS ****************************#
+#
+#    File:    conanfile.py
+#    Author:  Marvin Smith
+#    Date:    3/21/2025
+#
+from conan import ConanFile, tools
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
+from conan.tools.files import copy
+
+class ConanProject(ConanFile):
+
+    name = "terminus_calculator"
+    version = "0.0.1"
+
+    license = "MIT"
+    author  = "Marvin Smith <marvin.smith@terminus.com>"
+    url     = "https://www.github.org/marvins/calc-app"
+    description = "Embedded Calculator / Converter App"
+    topics = ("terminus","convert")
+
+    options = { "platform": ['desktop','pico'] }
+
+    default_options = { 'platform': 'desktop' }
+
+    settings = "os", "compiler", "build_type", "arch"
+
+    def build_requirements(self):
+        self.test_requires("gtest/1.15.0")
+
+    def requirements(self):
+        pass
+
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.configure()
+        return cmake
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["NAME_FROM_CONANFILE"]        = self.name
+        tc.variables["VERSION_FROM_CONANFILE"]     = self.version
+        tc.variables["DESCRIPTION_FROM_CONANFILE"] = self.description
+        tc.variables["URL_FROM_CONANFILE"]         = self.url
+
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        cmake = self._configure_cmake()
+        cmake.install()
+
+    def package_info(self):
+        #self.cpp_info.libs = ["terminus_nitf"]
+        pass
+
+    def package_id(self):
+        self.info.clear()
+
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,9 +1,12 @@
 
+// Project Libraries
+#include <tmns/app/calc/core/Options.hpp>
+
 
 int main( int argc, char* argv[] ){
 
     // Parse Command-Line and Configuration-File Options
-    auto config = 
+    auto config = Options::parse( argc, argv );
 
 
     return 0;
