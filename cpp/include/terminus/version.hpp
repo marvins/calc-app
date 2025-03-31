443ab8d6d3d8d9d88a512f99396b61c0cad2400a
Marvin Smith <marvin.smith@terminus-geo.com>
2025-03-31 00:19:47 -0600

    Updating version file.

--- cpp/include/terminus/version.hpp
+++ cpp/include/terminus/version.hpp
@@ -18,7 +18,7 @@
 
 namespace tmns {
 
-static constexpr std::string BUILD_DATE() { return "2025-03-30 23:07:02"; }
+static constexpr std::string BUILD_DATE() { return "2025-03-31 00:19:27"; }
 
 static constexpr std::string VERSION() { return "0.0.3"; }
 
